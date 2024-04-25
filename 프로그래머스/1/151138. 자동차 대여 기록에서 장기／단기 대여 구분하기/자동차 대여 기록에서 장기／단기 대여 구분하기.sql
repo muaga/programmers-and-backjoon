422ca00acf291a6386c6891bd3378aa148ff54bf
Eunhye Nam <muagaa@kakao.com>
2024-04-25 21:10:10 +0900

    Update 자동차 대여 기록에서 장기／단기 대여 구분하기.sql

--- 프로그래머스/1/151138. 자동차 대여 기록에서 장기／단기 대여 구분하기/자동차 대여 기록에서 장기／단기 대여 구분하기.sql
+++ 프로그래머스/1/151138. 자동차 대여 기록에서 장기／단기 대여 구분하기/자동차 대여 기록에서 장기／단기 대여 구분하기.sql
@@ -1,12 +1,18 @@
+-- date_format(컬럼, 포맷형식) : 날짜를 원하는 형태로 포맷 * 꼭 별칭을 달아줄 것
+-- datediff(끝 날짜, 시작날짜) : 날짜 차이를 일 수로 연산 * 꼭 끝 날짜를 앞에 둘 것
+-- year(컬럼), month(컬럼) : 데이터에서 각 형태의 데이터만 추출 
+
 SELECT 
-    HISTORY_ID,
-    CAR_ID,
-    DATE_FORMAT(START_DATE,'%Y-%m-%d') START_DATE,
-    DATE_FORMAT(END_DATE,'%Y-%m-%d') END_DATE,
-    CASE
-    WHEN DATEDIFF(END_DATE, START_DATE) + 1 >= 30 THEN '장기 대여'
-    ELSE '단기 대여'
-    END AS RENT_TYPE
-FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY 
-WHERE DATE_FORMAT(START_DATE, '%Y-%m') = '2022-09'
-ORDER BY HISTORY_ID DESC
+    history_id,
+    car_id,
+    date_format(start_date, '%Y-%m-%d') as start_date,
+    date_format(end_date, '%Y-%m-%d') as end_date,
+    case when datediff(end_date, start_date) + 1 >= 30 then '장기 대여'
+    -- 날짜 비교할 때, 꼭! + 1을 해 줄 것!!!!!
+        else '단기 대여'
+        end as rent_type
+from car_rental_company_rental_history
+-- where substring(start_date, 1, 2) = '2022-09'
+-- where date_format(start_date, '%y-%m') = '2022-09'
+where year(start_date) = 2022 and month(start_date) = 09
+order by history_id desc
