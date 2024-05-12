94e45b57b3f3846c471ba0d2377ed7129aa40a2a
Eunhye Nam <muagaa@kakao.com>
2024-05-13 05:04:38 +0900

    [level 0] Title: 문자열 곱하기, Time: 1.77 ms, Memory: 82.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String my_string, int k) {
+        String answer = "";
+        for (int n = 1; n <= k; n++) {
+                answer += my_string;
+        }
+        return answer;
+    }
+}
