8acd70731159c73fdded39e0cf0c2cf32aede7b0
Eunhye Nam <muagaa@kakao.com>
2024-05-16 02:33:54 +0900

    [level 0] Title: 배열 만들기 2, Time: 0.14 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
@@ -6,6 +6,11 @@ class Solution {
     public int[] solution(int l, int r) {
         List<Integer> numberList = new ArrayList<>();
         
+        // 5 50 55 500 505 550, 555 / 5
+        // 1 10 11 100 101 110 111
+        // 1 2 3 4 5 6 7
+        // byte네..........
+        
         int b = 1;
         
         for (int a = l; a <= r; a++) {
@@ -23,3 +28,9 @@ class Solution {
         return answer;
     }
 }
+
+        // 이진수 -> 숫자
+        // int decimal = Integer.parseInt(binary(string타입), 2);
+
+        // 숫자 -> 이진수
+        // String byte = Integer.toBinaryString(number(int타입));
