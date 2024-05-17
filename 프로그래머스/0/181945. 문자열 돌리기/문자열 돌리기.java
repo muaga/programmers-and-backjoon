47c9e47592baffe2bd520563cbfb5cc6d561d5e3
Eunhye Nam <muagaa@kakao.com>
2024-05-18 02:10:37 +0900

    [level 0] Title: 문자열 돌리기, Time: 130.69 ms, Memory: 67.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        for (int n = 0; n < a.length(); n++) {
+                System.out.println(a.charAt(n));
+        }
+    }
+}
