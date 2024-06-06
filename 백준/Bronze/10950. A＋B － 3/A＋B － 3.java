f3d8e8476e79df1d2f89b26e7945913daac0ed22
Eunhye Nam <muagaa@kakao.com>
2024-06-06 22:52:20 +0900

    [Bronze V] Title: A+B - 3, Time: 228 ms, Memory: 17884 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        for (int n = 0; n < t; n++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(a + b);
+        }
+
+        sc.close();
+    }
+}
