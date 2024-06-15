54d04bde7646ee880b773af8c712f0ebbd268be2
Eunhye Nam <muagaa@kakao.com>
2024-06-15 15:26:51 +0900

    [Bronze V] Title: 개수 세기, Time: 216 ms, Memory: 17848 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,15 +6,21 @@ public class Main {
         int n = sc.nextInt();
 
         int[] arr = new int[n];
-        for(int c = 0; c < n; c++){
+        for (int c = 0; c < n; c++) {
             int number = sc.nextInt();
             arr[c] = number;
         }
 
         int v = sc.nextInt();
-        
-        long count = Arrays.stream(arr).filter(num -> num == v).count();
-        System.out.println(count);
+
+        int sum = 0;
+        for (int number : arr) {
+            if (number == v) {
+                sum += 1;
+            }
+        }
+
+        System.out.println(sum);
         sc.close();
     }
 }
