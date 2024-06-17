a4150973ceea94585457917ceafce02b9d437525
Eunhye Nam <muagaa@kakao.com>
2024-06-18 00:12:10 +0900

    [Bronze III] Title: 최댓값, Time: 152 ms, Memory: 16016 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -5,28 +5,20 @@ import java.util.stream.Collectors;
 public class Main {
 
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int[] numbers = new int[9]; // 크기가 9인 배열 생성
+        int[] numbers = new int[9];
 
-        // 각 입력마다 한 줄씩 자연수 입력 받기
-        for (int i = 0; i < 9; i++) {
+        for (int i = 0; i < numbers.length; i++) {
             numbers[i] = Integer.parseInt(br.readLine());
         }
 
-        // 최댓값 찾기
         int max = Arrays.stream(numbers).max().getAsInt();
-
-        // 최댓값의 인덱스 찾기 (1부터 시작하는 인덱스)
-        int maxIndex = Arrays.stream(numbers)
-                             .boxed()
-                             .collect(Collectors.toList())
-                             .indexOf(max) + 1;
-
-        // 최댓값과 최댓값의 인덱스 출력
+        int idx = Arrays.stream(numbers).boxed().collect(Collectors.toList()).indexOf(max) + 1;
         bw.write(max + "\n");
-        bw.write(maxIndex + "\n");
+        bw.write(idx + "\n");
 
         br.close();
         bw.flush();
