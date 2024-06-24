7748b8528b82c2a9ac21c6cbb8c63a53c8c6a390
Eunhye Nam <muagaa@kakao.com>
2024-06-24 16:24:13 +0900

    [Bronze III] Title: 공 넣기, Time: 156 ms, Memory: 15968 KB -BaekjoonHub

--- 백준/Bronze/10810. 공 넣기/공 넣기.java
+++ 백준/Bronze/10810. 공 넣기/공 넣기.java
@@ -1,25 +1,40 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        int m = sc.nextInt();
 
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        // 첫 번째 줄에서 n과 m을 읽음
+        String[] firstLine = br.readLine().split(" ");
+        int n = Integer.parseInt(firstLine[0]);
+        int m = Integer.parseInt(firstLine[1]);
+
+        int[] arr = new int[n];
+
+        // m번 공을 넣는 작업 수행
         for (int c = 0; c < m; c++) {
-            int i = sc.nextInt();
-            int j = sc.nextInt();
-            int k = sc.nextInt();
+            String[] input = br.readLine().split(" ");
+            int i = Integer.parseInt(input[0]);
+            int j = Integer.parseInt(input[1]);
+            int k = Integer.parseInt(input[2]);
+
+            // i번 바구니부터 j번 바구니까지 k번 공을 넣음
             for (int a = i; a <= j; a++) {
-                arr[a - 1] = k;
+                arr[a - 1] = k; // 배열 인덱스는 0부터 시작하므로 i-1, j-1로 접근
             }
         }
 
+        // 결과 출력
         for (int ball : arr) {
-            System.out.print(ball + " ");
+            bw.write(ball + " ");
         }
 
-        sc.close();
+        br.close();
+        bw.flush();
+        bw.close();
     }
 }
