1497d43fa0247278db0c19126076d7ddfb67048d
Eunhye Nam <muagaa@kakao.com>
2024-06-24 17:36:00 +0900

    [Bronze II] Title: 공 바꾸기, Time: 152 ms, Memory: 16260 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -1,17 +1,26 @@
+import java.io.*;
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        // 한 줄 입력 시, 공백을 기준으로 입력 값 배열로 담기
+        String[] firstLine = br.readLine().split(" ");
+        int n = Integer.parseInt(firstLine[0]);
+        int m = Integer.parseInt(firstLine[1]);
         int[] arr = new int[n];
+
+        // 배열 값 초기화
         Arrays.setAll(arr, i -> i + 1);
 
         for (int c = 0; c < m; c++) {
-            int i = sc.nextInt();
-            int j = sc.nextInt();
+            String[] secondLine = br.readLine().split(" ");
+            int i = Integer.parseInt(secondLine[0]);
+            int j = Integer.parseInt(secondLine[1]);
 
             // 변경 전 데이터 깊은 복사
             int iNum = arr[i - 1];
@@ -22,10 +31,13 @@ public class Main {
             arr[j - 1] = iNum;
         }
 
+        // for문 돌면서 배열 값 추출
         for (int ball : arr) {
-            System.out.print(ball + " ");
+            bw.write(ball + " ");
         }
 
-        sc.close();
+        br.close();
+        bw.flush();
+        bw.close();
     }
 }
